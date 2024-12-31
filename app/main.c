2b0e13e28af641318466ab54fd4de7dce79a03c9
TonyMJ <tonymj@proton.me>
2024-12-31 17:01:19 +0100

    echo_command

--- app/main.c
+++ app/main.c
@@ -13,13 +13,29 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
 
-    input[strcspn(input, "\n")] = '\0';
+    input[strcspn(input, "\n")] = ' ';
 
-    char exit_command[]="exit 0";
+    const char s[4] = " ";
+    char* cmd;
+    cmd = strtok(input, s);
+    // char tokenized_input[] = strok(input, " ");
 
-    if (strcmp(input, exit_command) == 0) {
+    char exit_command[]="exit";
+    char echo_command[]="echo";
+
+    if (strcmp(cmd, exit_command) == 0) {
       running=0;
       break;
+    }
+
+    char* args;
+    args = strtok(0, s);
+    if (strcmp(input, echo_command) == 0) {
+      while (args != 0) {
+        printf("%s ", args);
+        args = strtok(0, s);
+      }
+      printf("\n");
     }
 
     else {
