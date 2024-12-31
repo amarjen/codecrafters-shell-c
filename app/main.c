ff90043810e37b4aeaefd068cb1a80935c6bf337
TonyMJ <tonymj@proton.me>
2024-12-31 16:40:30 +0100

    repl

--- app/main.c
+++ app/main.c
@@ -5,6 +5,8 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  int running = 1;
+  while (running) {
   // Uncomment this block to pass the first stage
   printf("$ ");
 
@@ -13,6 +15,11 @@ int main() {
   fgets(input, 100, stdin);
 
   input[strcspn(input, "\n")] = '\0';
-  printf("%s: command not found\n", input);
+
+  char quit_command[]="quit";
+
+  if (strcmp(input, quit_command) == 0) {running=0; break;}
+  else { printf("%s: command not found\n", input); }
+  }
   return 0;
 }
